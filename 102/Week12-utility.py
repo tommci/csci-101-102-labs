80f4b84e525776e1d74ef70c74de99e47ea522e9
tommci <129996514+tommci@users.noreply.github.com>
2023-04-11 00:51:49 -0600

    Added update_string function

--- 102/Week12-utility.py
+++ 102/Week12-utility.py
@@ -8,3 +8,12 @@ def load_file(filename):
     with open(filename, 'r') as f:
         content = f.read().splitlines()     # splitlines gets rid of the /n in the output
     return content
+
+def update_string(string1, string2, index):
+    output_str = ''
+    for i in range(0, len(string1)):
+        if i == index:
+            output_str += string2
+        else:
+            output_str += string1[i]
+    print(f'OUTPUT {output_str}')
